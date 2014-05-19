4adf1aa7430d40abc58183b7b05a71b2546653d6
prachidhabu <prachi@cuberoot.in>
2014-05-19 14:00:05 +0530

    added spec for user invitation

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Users" do
+describe "Admin" do
   before(:each) do
     @user = FactoryGirl.create(:user)
     login(@user)
@@ -11,6 +11,47 @@ describe "Users" do
     visit users_path
 
     # then
-    page.should have_link 'Invite User'
+    page.should have_link 'Invite Employee'
   end
+
+  it "should invite the new employee" do
+
+    # given
+      visit users_path
+
+    # when
+      click_link 'Invite Employee'
+
+    # then
+      page.current_path.should eq(new_user_invitation_path)
+
+    # when
+      fill_in "Email", :with => "example@cuberoot.in"
+      click_button "Send an invitation"
+
+    # then
+      page.current_path.should eq(root_path)
+      page.should have_content "An invitation email has been sent to example@cuberoot.in."
+  end
+
+  it "invitation failed for invalid email" do
+     # given
+      visit users_path
+
+    # when
+      click_link 'Invite Employee'
+
+    # then
+      page.current_path.should eq(new_user_invitation_path)
+
+    # when
+      fill_in "Email", :with => "example@cuberoot.com"
+      click_button "Send an invitation"
+
+    # then
+      page.current_path.should eq(user_invitation_path)
+      page.should have_content "should be a cuberoot.in email"
+  end
+
+
 end
