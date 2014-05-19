8aeb6702a440477678d13740031940aa2b9fbb01
prachidhabu <prachi@cuberoot.in>
2014-05-19 15:35:03 +0530

    permitted manager_id in devise invitation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519062409) do
+ActiveRecord::Schema.define(version: 20140519083618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20140519062409) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
+    t.integer  "manager_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
