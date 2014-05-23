60b9e96ca4340516e8a6781826a1b5b27b6ef2c7
prachidhabu <prachi@cuberoot.in>
2014-05-23 14:25:23 +0530

    added leave_mailer

---
+++ app/mailers/leave_mailer.rb
@@ -0,0 +1,17 @@
+class LeaveMailer < ActionMailer::Base
+
+  def notify_pending_leave(leave)
+    @leave = leave
+    @user = @leave.user
+    @mail_to = @user.manager.email
+    @subject = "New Leave Application By- @user.name"
+    @from = @user.email
+    setup_email(@mail_to, @subject, @from)
+  end
+
+  protected
+    def setup_email(mail_to, subject, from)
+      mail(:to => mail_to, :subject => "[CubeRoot LTS] " + subject, :date => Time.now, :from => from)
+    end
+
+end
