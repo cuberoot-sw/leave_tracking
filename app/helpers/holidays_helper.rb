8f25d68adcfeda9cc7a54b11f4c7271424c36a8a
prachidhabu <prachi@cuberoot.in>
2014-06-26 17:48:03 +0530

    optimized code with helper

--- app/helpers/holidays_helper.rb
+++ app/helpers/holidays_helper.rb
@@ -1,2 +1,6 @@
 module HolidaysHelper
+  def select_year_options(year)
+    year_list = Setting.all.order('year DESC')
+    options_from_collection_for_select(year_list, 'year', 'year', year)
+  end
 end
