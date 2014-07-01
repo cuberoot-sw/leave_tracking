3273e6b587a39bc0773d05b4f1a67659f20eeff6
prachidhabu <prachi@cuberoot.in>
2014-07-01 12:34:01 +0530

    added pagination on resoorces on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
+  require 'will_paginate/array'
   def index
     if current_user.present?
       @total_leaves, @balance_leaves= current_user.collect_leaves_count
       @birthdays = User.birthdays
-      @resources = current_user.is_employee? ? [] : current_user.resources
+      @resources = current_user.is_employee? ? [] : current_user.resources.paginate(:page => params[:page], :per_page => 5)
     end
   end
 end
